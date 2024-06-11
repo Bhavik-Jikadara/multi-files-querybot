43b3a1edee6cda8db3627c281e59cca0461e8fff
Bhavik Jikadara <bhavikjikadara33523@gmail.com>
2024-06-11 16:35:03 +0530

    Resolve image issues in htmlTemplates.py

--- src/htmlTemplates.py
+++ src/htmlTemplates.py
@@ -28,7 +28,7 @@ css = '''
 bot_template = '''
 <div class="chat-message bot">
     <div class="avatar">
-        <img src="./images/agent.png">
+        <img src="https://github.com/Bhavik-Jikadara/multiple-pdfs-querybot/blob/main/src/images/agent.png?raw=true" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
     </div>
     <div class="message">{{MSG}}</div>
 </div>
@@ -37,7 +37,7 @@ bot_template = '''
 user_template = '''
 <div class="chat-message user">
     <div class="avatar">
-        <img src="./images/user.png">
+        <img src="https://github.com/Bhavik-Jikadara/multiple-pdfs-querybot/blob/main/src/images/user.png?raw=true" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
     </div>    
     <div class="message">{{MSG}}</div>
 </div>
