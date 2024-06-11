47ad4ba19aa05d8984e5dfc000a4d0e8f3632d61
Bhavik Jikadara <66177876+Bhavik-Jikadara@users.noreply.github.com>
2024-06-11 16:49:31 +0530

    Update htmlTemplates.py

--- src/htmlTemplates.py
+++ src/htmlTemplates.py
@@ -22,7 +22,7 @@ css = '''
 bot_template = '''
 <div class="chat-message bot">
     <div class="avatar">
-        <img src="https://github.com/Bhavik-Jikadara/multiple-pdfs-querybot/blob/main/src/images/agent.png?raw=true" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+        <img src="https://raw.githubusercontent.com/Bhavik-Jikadara/multiple-pdfs-querybot/main/src/images/agent.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
     </div>
     <div class="message">{{MSG}}</div>
 </div>
@@ -31,7 +31,7 @@ bot_template = '''
 user_template = '''
 <div class="chat-message user">
     <div class="avatar">
-        <img src="https://github.com/Bhavik-Jikadara/multiple-pdfs-querybot/blob/main/src/images/user.png>raw=true" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+        <img src="https://raw.githubusercontent.com/Bhavik-Jikadara/multiple-pdfs-querybot/main/src/images/user.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
     </div>    
     <div class="message">{{MSG}}</div>
 </div>
